d8d0e7782b074e051d09f1e4a2b059ebdc19767c
Dan Payne <dpayne5532@gmail.com>
2023-02-06 15:58:26 -0700

    refactor code

--- iExpense2/ContentView.swift
+++ iExpense2/ContentView.swift
@@ -8,57 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var expenses = Expenses()
-    @State private var showingAddExpense = false
-    
-    
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(expenses.items, id: \.id) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                            
-                            
-                            
-                        }
-                        Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
-                        
-                        
-                    }
-                    
-                    
-                }
-                .onDelete(perform: removeItems)
+  @StateObject var expenses = Expenses()
+  @State private var showingAddExpense = false
+  
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(expenses.items, id: \.id) { item in
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+                .font(.headline)
+              Text(item.type)
+              
+              
+              
             }
-            .navigationTitle("iExpense")
-            .toolbar {
-                Button {
-                   showingAddExpense = true
-                } label: {
-                    Image(systemName: "plus")
-                }
-            }
+            Spacer()
+            Text(item.amount, format: .currency(code: "USD"))
             
-            .sheet(isPresented: $showingAddExpense) {
-                AddView(expenses: expenses)
-            }
-         
             
+          }
+          
+          
         }
-        
+        .onDelete(perform: removeItems)
+      }
+      .navigationTitle("iExpense")
+      .toolbar {
+        Button {
+          showingAddExpense = true
+        } label: {
+          Image(systemName: "plus")
+        }
+      }
+      
+      .sheet(isPresented: $showingAddExpense) {
+        AddView(expenses: expenses)
+      }
+      
+      
     }
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
-    }
+    
+  }
+  func removeItems(at offsets: IndexSet) {
+    expenses.items.remove(atOffsets: offsets)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- iExpense2/Expenses.swift
+++ iExpense2/Expenses.swift
@@ -9,24 +9,24 @@ import Foundation
 
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]() {
-        didSet {
-            let encoder = JSONEncoder()
-            
-            if let encoded = try? encoder.encode(items) {
-                UserDefaults.standard.set(encoded, forKey: "Items")
-            }
-        }
+  @Published var items = [ExpenseItem]() {
+    didSet {
+      let encoder = JSONEncoder()
+      
+      if let encoded = try? encoder.encode(items) {
+        UserDefaults.standard.set(encoded, forKey: "Items")
+      }
     }
-    init() {
-        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
-            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
-                items = decodedItems
-                return
-            }
-        }
-        
-        items = []
+  }
+  init() {
+    if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+      if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+        items = decodedItems
+        return
+      }
     }
-   
+    
+    items = []
+  }
+  
 }
